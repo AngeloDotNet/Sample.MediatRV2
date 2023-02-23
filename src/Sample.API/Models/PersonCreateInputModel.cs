a4075cc61e229dabcbc08baa559e335a6842d7f1
Angelo Pirola <angelo9871@gmail.com>
2023-02-23 14:01:13 +0100

    Aggiunti Input models

---
+++ src/Sample.API/Models/PersonCreateInputModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Sample.API.Models;
+
+public class PersonCreateInputModel
+{
+    public Guid UserId { get; set; }
+    public string Cognome { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+
+    public PersonCreateInputModel(Guid userId, string cognome, string nome, string email)
+    {
+        UserId = userId;
+        Cognome = cognome;
+        Nome = nome;
+        Email = email;
+    }
+}
---
+++ src/Sample.API/Models/PersonUpdateInputModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Sample.API.Models;
+
+public class PersonUpdateInputModel
+{
+    public int Id { get; set; }
+    public Guid UserId { get; set; }
+    public string Cognome { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+
+    public PersonUpdateInputModel(int id, Guid userId, string cognome, string nome, string email)
+    {
+        Id = id;
+        UserId = userId;
+        Cognome = cognome;
+        Nome = nome;
+        Email = email;
+    }
+}
