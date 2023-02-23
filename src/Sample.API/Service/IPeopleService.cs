dc1e6c05ad73fc01a04b51775894e150d6e082aa
Angelo Pirola <angelo9871@gmail.com>
2023-02-23 14:00:58 +0100

    Aggiunti Service

---
+++ src/Sample.API/Service/IPeopleService.cs
@@ -0,0 +1,10 @@
+﻿namespace Sample.API.Service;
+
+public interface IPeopleService
+{
+    Task<List<PersonEntity>> GetPeopleAsync();
+    Task<PersonEntity> GetPersonAsync(int id);
+    Task CreatePersonAsync(PersonEntity person);
+    Task UpdatePersonAsync(PersonEntity person);
+    Task DeletePersonAsync(PersonEntity person);
+}
