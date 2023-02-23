bd0522c87ef7b5364027ab4e720810bddaacb2f2
Angelo Pirola <angelo9871@gmail.com>
2023-02-23 13:58:09 +0100

    Aggiunte entità

---
+++ src/Sample.API/Entity/PersonEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Sample.API.Entity;
+
+public class PersonEntity
+{
+    public int Id { get; set; }
+    public Guid UserId { get; set; }
+    public string Cognome { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+}
