c010a385e79f26bbc22f32631583144e471bb464
Angelo Pirola <angelo9871@gmail.com>
2023-02-23 13:57:53 +0100

    Aggiunto nuovo Controller

---
+++ src/Sample.API/Controllers/PeopleController.cs
@@ -0,0 +1,63 @@
+﻿namespace Sample.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+public class PeopleController : ControllerBase
+{
+    private readonly ILogger<PeopleController> logger;
+    private readonly IMediator mediator;
+
+    public PeopleController(ILogger<PeopleController> logger, IMediator mediator)
+    {
+        this.logger = logger;
+        this.mediator = mediator;
+    }
+
+    [HttpGet("people")]
+    public async Task<IActionResult> GetPeople()
+    {
+        var people = await mediator.Send(new GetPeopleListQuery());
+        return Ok(people);
+    }
+
+    [HttpGet("person/{id}/{userId}")]
+    public async Task<IActionResult> GetPerson(int id, Guid userId)
+    {
+        var person = await mediator.Send(new GetPersonQuery(id, userId));
+
+        if (person == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(person);
+    }
+
+    [HttpPost("person")]
+    public async Task<IActionResult> CreatePerson([FromBody] PersonCreateInputModel person)
+    {
+        var result = await mediator.Send(new CreatePersonCommand(person));
+        return Ok(result);
+    }
+
+    [HttpPut("person")]
+    public async Task<IActionResult> UpdatePerson([FromBody] PersonUpdateInputModel person)
+    {
+        var result = await mediator.Send(new UpdatePersonCommand(person));
+        return Ok(result);
+    }
+
+    [HttpDelete("person/{id}")]
+    public async Task<IActionResult> DeletePerson(int id)
+    {
+        var result = await mediator.Send(new DeletePersonCommand(id));
+
+        if (!result)
+        {
+            return NotFound();
+        }
+
+        return Ok();
+    }
+}
